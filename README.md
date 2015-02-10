7a3107962a59cb22c20ddfed4980e71a8a025bf3
maokejackson <maokejackson@gmail.com>
2015-02-10 22:39:42 +0800

    Modify to export file diff of two tags

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 svn-export-changes.sh
 =====================
 
-Bash script for exporting svn files from a range of revisions
+Bash script for exporting svn changed files between two locations (Normally for tags).
 
 It can sometimes be a pain to export just modified files from SVN on the MAC/*nix machines. 
 
@@ -15,13 +15,15 @@ Usage
 
 Download this and chmod 755 it. Add it to your bin if you so desire, then it's as simple as 
 
-`svn-export-changes repo-location`
+`svn-export-changes repo-location old-location new-location`
 
-The script will pull the changes to that repository between the HEAD and PREV and put it in a directory called 'export' (relative to the script location)
+e.g. svn-export-changes svn://localhost/repository tags/1.0 tags/2.0
 
-You can pass in a range of revisions and a location to export to.
+The script will pull the changes to that repository between two locations and put it in a directory called 'export' (relative to the script location)
 
-`svn-export-changes repo-location revision-from revision-to export-directory`
+You can pass in export directory and log file to export to.
+
+`svn-export-changes repo-location tags/old-version tags/new-version export-directory log-file`
 
 That's it.
 
--- svn-export-changes.sh
+++ svn-export-changes.sh
@@ -2,19 +2,32 @@
 
 if [ ! $1 ]; then
     echo "Please enter an SVN repository location"
-    exit
+    exit 1
+fi
+
+if [ ! $2 ]; then
+    echo "Please enter an older directory in SVN"
+    exit 1
+fi
+
+if [ ! $3 ]; then
+    echo "Please enter a newer directory in SVN"
+    exit 1
 fi
 
 # set up nice names for the incoming parameters to make the script more readable
 repository=$1
-revision_from=$2
-revision_to=$3
+dir_from=$2
+dir_to=$3
 target_directory=$4
+change_log=$5
 
-: ${revision_to:="HEAD"} 
-: ${revision_from:="PREV"}
-: ${target_directory:="export"} 
+: ${target_directory:="export"}
+: ${change_log:="change.log"}
 
+path_from=$repository/$dir_from
+path_to=$repository/$dir_to
+path_log=$target_directory/$change_log
 
 # the grep is needed so we only get added/modified files and not the deleted ones or anything else
 # if it's a modified directory it's " M" so won't show with this command (good)
@@ -23,29 +36,33 @@ target_directory=$4
 # clear out directory first
 rm -R $target_directory
 
-echo -e "\033[38;5;148mCleaning export directory\033[39m"
+echo "\033[38;5;148mCleaning export directory\033[39m"
 
 # Begin export
-echo -e "\033[00;31mExporting files revised between $revision_from and $revision_to to /$target_directory\033[39m"
+echo "\033[00;31mExporting files revised between $path_from and $path_to to /$target_directory\033[39m"
 
-for line in `svn diff --summarize -r$revision_from:$revision_to $repository | grep "^[AM]"`
+for line in `svn diff --summarize -r HEAD --old=$path_from --new=$path_to | grep "^[AM]"`
 do
     # each line in the above command in the for loop is split into two:
     # 1) the status line (containing A, M, AM, D etc)
     # 2) the full repository and filename string
     # so only export the file when it's not the status line
     if [ $line != "A" ] && [ $line != "AM" ] && [ $line != "M" ]; then
+        # replace with new location because svn diff shows the changes of old location
+        repo_path=`echo "$line" |sed "s|$path_from|$path_to|g"`
         # use sed to remove the repository from the full repo and filename
-        filename=`echo "$line" |sed "s|$repository||g"`
+        filename=`echo "$repo_path" | sed "s|$path_to||g"`
         # don't export if it's a directory we've already created
-        if [ ! -d $target_directory$filename ]; then
-            directory=`dirname $filename`
-            mkdir -p $target_directory$directory
+        file_path=`echo $target_directory$filename | sed "s|%20| |g"` # replace %20 with space
+        if [ ! -d "$file_path" ]; then
+            directory=`dirname "$filename" | sed "s|%20| |g"` # replace %20 with space
+            mkdir -p "$target_directory$directory"
             echo "$filename"
-            svn export -qr $revision_to $line $target_directory$filename
+            svn export -qr HEAD $repo_path "$file_path"
         fi
     fi
 done
 
-# to summarize any deleted files or directories at the end of the script uncomment the following line
-#svn diff --summarize -r$revision_from:$revision_to $repository | grep "^[D]"
+# create a change log indicating which files are added/modified/deleted
+svn diff --summarize -r HEAD --old=$path_from --new=$path_to | sed "s|$path_from/||g" | sed 's/%20/ /g' > $path_log
+echo "\033[38;5;148mChange log has been created at $path_log\033[39m"
